feb342b6e431f979695994ac1abac09fb5b308eb
ronalds <ronaldsronix@gmail.com>
2024-11-07 12:20:37 +0200

    .

--- frontend/ytter/src/ShouldntShow.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ShouldntShow() {
-  return <h2>This shouldnt show</h2>;
-}
-
-export default ShouldntShow;
--- frontend/ytter/src/components/Nono.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-function Nono(){
-    return <h2>Nono</h2>;
-}
-
-export default Nono;
